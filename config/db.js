32b08df004764ee0ee3e0216570e6dbdebd953f3
Ravinder <ravinder.s.negi@indusnet.co.in>
2023-09-24 17:06:31 +0530

    test ssl

--- config/db.js
+++ config/db.js
@@ -1,13 +1,30 @@
 const { Client } = require("pg");
 const { host, user, port, password, database } = require("../utils/config");
 
-const client = new Client({
-  host: host,
-  user: user,
-  port: port,
-  password: password,
-  database: database,
-});
+let client;
+
+if (process.env.NODE_ENV === "production") {
+  // Production database connection
+  client = new Client({
+    host: "production_host",
+    user: "production_user",
+    port: "production_port",
+    password: "production_password",
+    database: "production_database",
+    ssl: {
+      rejectUnauthorized: true, // You should enable SSL for production
+    },
+  });
+} else {
+  // Development or other environments
+  client = new Client({
+    host: host,
+    user: user,
+    port: port,
+    password: password,
+    database: database,
+  });
+}
 
 client.on("connection", (err) => {
   console.error("Postgres connection error", err);

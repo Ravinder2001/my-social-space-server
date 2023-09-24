06ea87cd79df108450b917cb57634aaa4e07f42e
Ravinder <ravinder.s.negi@indusnet.co.in>
2023-09-24 16:53:35 +0530

    changes

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const Messages_Routes = require("./routes/messages.routes");
 
 app.use(
   cors({
-    origin: ["https://my-social-space.vercel.app/"],
+    origin: [config.domain],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })

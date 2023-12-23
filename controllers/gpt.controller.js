b3c597dec72a97be78a1437b1e4ec4f4d971d291
Ravinder <ravinder.s.negi@indusnet.co.in>
2023-12-23 20:49:58 +0530

    suggestion added

--- controllers/gpt.controller.js
+++ controllers/gpt.controller.js
@@ -3,35 +3,45 @@ const { Bad, Success } = require("../utils/constant");
 
 const openai = new OpenAI();
 module.exports = {
-  Post_Caption_Generator: async (req, res) => {
+  Generator_Caption: async (req, res) => {
+    try {
+      // const completion = await openai.chat.completions.create({
+      //   messages: [
+      //     { role: "system", content: "You are caption generator assistant and your response should be inside 255 characters." },
+      //     {
+      //       role: "user",
+      //       content: `Generate a captivating caption for the following post:${req.body.post}.`,
+      //     },
+      //   ],
+      //   model: "gpt-3.5-turbo",
+      //   // max_tokens: 30,
+      // });
+
+      // res.status(Success).json({ data: completion.choices[0].message?.content, status: Success });
+      res.status(Success).json({
+        data: '"Experiencing the serene beauty of Kedarnath with my soulmates – moments that will last a lifetime! 🏔️✨ #KedarnathTrip #UnforgettableJourneys #CherishingEveryMoment"',
+        status: Success,
+      });
+    } catch (err) {
+      res.status(Bad).json({ message: err.message, status: Bad });
+    }
+  },
+  Generator_Suggestion: async (req, res) => {
     try {
       const completion = await openai.chat.completions.create({
         messages: [
-          { role: "system", content: "You are caption generator assistant." },
+          { role: "system", content: "You are message reply suggestion generator assistant and response should be inside 50 characters." },
           {
             role: "user",
-            content: `Generate a captivating caption for the following post:${req.body.post}.
-                    and the caption should be under 255 words only.`,
+            content: `Generate a reply suggestion for the following message:${req.body.message}.`,
           },
         ],
         model: "gpt-3.5-turbo",
-        // max_tokens: 30,
+        // max_tokens: 10,
       });
 
-      res.status(Success).json({ data: completion.choices[0], status: Success });
-      //   res.status(Success).json({
-      //     data: {
-      //       index: 0,
-      //       message: {
-      //         role: "assistant",
-      //         content:
-      //           '"Experiencing the serene beauty of Kedarnath with my soulmates – moments that will last a lifetime! 🏔️✨ #KedarnathTrip #UnforgettableJourneys #CherishingEveryMoment"',
-      //       },
-      //       logprobs: null,
-      //       finish_reason: "stop",
-      //     },
-      //     status: Success,
-      //   });
+      res.status(Success).json({ data: completion.choices[0].message?.content?.replace(/['"]/g, ''), status: Success });
+      // res.status(Success).json({ data: "I'm ssssfine, thanks.", status: Success });
     } catch (err) {
       res.status(Bad).json({ message: err.message, status: Bad });
     }
--- routes/gpt.routes.js
+++ routes/gpt.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { Post_Caption_Generator } = require("../controllers/gpt.controller");
+const { Generator_Caption, Generator_Suggestion } = require("../controllers/gpt.controller");
 const authentication = require("../helpers/JWT/authentication");
 const router = express.Router();
 
-router.post("/generate_caption", authentication, Post_Caption_Generator);
+router.post("/generate_caption", authentication, Generator_Caption);
+router.post("/generate_suggestion", authentication, Generator_Suggestion);
 
 module.exports = router;

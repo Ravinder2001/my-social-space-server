76d9496007580634f2d887014e1c6812587be229
Ravinder <rvnegi786@gmail.com>
2024-07-28 16:14:43 +0530

    removed

--- routes/gpt.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { Generator_Caption, Generator_Suggestion } = require("../controllers/gpt.controller");
-const authentication = require("../helpers/JWT/authentication");
-const router = express.Router();
-
-router.post("/generate_caption", authentication, Generator_Caption);
-router.post("/generate_suggestion", authentication, Generator_Suggestion);
-
-module.exports = router;

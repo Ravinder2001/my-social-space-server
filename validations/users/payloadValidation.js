292d350859a66e2939e4e72285adae9762cff2c0
Ravinder <rvnegi786@gmail.com>
2024-12-05 09:39:21 +0530

    formatted code

--- validations/users/payloadValidation.js
+++ validations/users/payloadValidation.js
@@ -10,10 +10,6 @@ module.exports = {
       "any.required": "Username is required",
     }),
 
-
-
-
-    
     email: Joi.string().email().required().messages({
       "string.base": "Email must be a string",
       "string.email": "Email must be a valid email address",

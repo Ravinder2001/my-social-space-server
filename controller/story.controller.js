d79ba5330b27d8e3cabcd20506702f0212df9174
Ravinder Singh Negi <ravinder.s.negi@intglobal.com>
2025-05-04 09:22:20 +0530

    New data format

--- controller/story.controller.js
+++ controller/story.controller.js
@@ -28,7 +28,7 @@ module.exports = {
   getUserStories: asyncHandler(async (req) => {
     const stories = await storyModel.getUserStories(req.user.user_id);
 
-    const updatedStories = await Promise.all(
+    const processedStories = await Promise.all(
       stories.map(async (story) => {
         if (story.media_url) {
           story.media_url = await generatePreSignedURL(story.media_url);
@@ -39,15 +39,38 @@ module.exports = {
         if (story.user_id == req.user.user_id) {
           story.ownStory = true;
           story.user_name = "You";
+        } else {
+          story.user_name = story.user_name.split(" ")[0];
         }
         delete story.user_id;
         return story;
       })
     );
 
+    // Grouping by user_id
+    const userMap = new Map();
+
+    for (const story of processedStories) {
+      const { user_id, user_name, profile_picture, ownStory, ...storyDetails } = story;
+
+      if (!userMap.has(user_id)) {
+        userMap.set(user_id, {
+          name: user_name,
+          user_id,
+          profile_picture,
+          ownStory: !!ownStory,
+          stories: [],
+        });
+      }
+
+      userMap.get(user_id).stories.push(storyDetails);
+    }
+
+    const groupedStories = Array.from(userMap.values());
+
     return {
       message: "Story added successfully",
-      data: updatedStories,
+      data: groupedStories,
       status: 200,
     };
   }),
